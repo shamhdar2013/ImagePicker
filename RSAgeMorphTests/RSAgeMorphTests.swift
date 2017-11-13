5972616aec96103fcd00d8055c51720ad981cd7d
RADHIKA SHARMA <radhikasharma@RADHIKAs-MacBook-Pro.local>
2017-11-13 14:52:13 -0800

    Initial Commit

---
+++ RSAgeMorphTests/RSAgeMorphTests.swift
@@ -0,0 +1,36 @@
+//
+//  RSAgeMorphTests.swift
+//  RSAgeMorphTests
+//
+//  Created by RADHIKA SHARMA on 11/13/17.
+//  Copyright © 2017 RADHIKA SHARMA. All rights reserved.
+//
+
+import XCTest
+@testable import RSAgeMorph
+
+class RSAgeMorphTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
